927006642943898b51db4cfe3b990cd531317a25
Sam Howard <essjay89@hotmail.co.uk>
2023-04-28 11:00:35 +0100

    Completed scooter class

--- src/Scooter.js
+++ src/Scooter.js
@@ -1,6 +1,30 @@
 class Scooter{
-  // scooter code here
-}
+  constructor (station, user, serial, nextSerial, charge, isBroken){
+    this.station = station;
+    this.user = user;
+    this.serial = serial;
+    this.nextSerial = nextSerial;
+    this.charge = charge;
+    this.isBroken = isBroken;
+  }
 
+  rent () {
+    if (this.charge > 20 && this.isBroken == false){
+      this.station = null;
+      this.user = user;
+    } 
+    
+    else {
+      throw new Error('Scooter needs to charge or scooter needs repair!');
+    }
+
+  }
+
+  dock (station) {
+    this.station = station;
+    this.user = null;
+  }
+
+}
 
 module.exports = Scooter
