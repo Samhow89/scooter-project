81a78c1c9116bfea58bdea10acd596227435f2bd
Sam Howard <essjay89@hotmail.co.uk>
2023-04-28 14:09:05 +0100

    added methods

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -2,7 +2,59 @@ const User = require('./User')
 const Scooter = require('./Scooter')
 
 class ScooterApp {
-  // ScooterApp code here
+  constructor (stations, registeredUsers){
+    this.stations = stations;
+    this.registeredUsers = registeredUsers;
+  }
+
+  registerUser (username, password, age) {
+    if (username in this.registeredUsers) {
+      throw new Error("User already registered");
+    }
+    else if (age < 18) {
+      throw new Error("User is too young to register");
+    }
+    else {
+  
+      this.registeredUsers.push({
+        username: username,
+        password: password,
+        age: age
+      });
+
+      console.log("User has been registered");
+    }
+  }
+
+  loginUser (username, password) {
+
+  }
+
+  logoutUser (username) {
+
+  }
+
+  createScooter (station) {
+
+  }
+
+  dockScooter (scooter, station) {
+
+  }
+
+  rentScooter (scooter, user) {
+
+  }
+
+  print () {
+    console.log(this.stations)
+    console.log(this.registeredUsers)
+  }
+
+
+
+
 }
+
 
 module.exports = ScooterApp
--- src/User.js
+++ src/User.js
@@ -1,5 +1,25 @@
 class User {
-  // User code here
+  constructor (username, password, age, loggedIn){
+    this.username = username;
+    this.password = password;
+    this.age = age;
+    this.loggedIn = loggedIn;
+  }
+
+  login (password) {
+
+    if (password == this.password){
+      this.loggedIn = true;
+    }
+
+    else {
+      throw new Error('Incorrect password');
+    }
+   }
+
+   logout () {
+    this.loggedIn = false;
+   }
 }
 
 module.exports = User
